04a46c588955fccddcb89df73c8470a211779ec2
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-03 14:08:28 +0530

    Coza-Loza-Voza with % conditions

---
+++ CozaLozaWoza.java
@@ -0,0 +1,38 @@
+public class CozaLozaWoza {
+    public static void main(String[] args) {
+        int num = 110;
+
+        for (int i = 1; i < num; i++) {
+            if(i%3==0 && i%5==0 && i%7==0){
+                System.out.print("CozaLozaWoza  ");
+            }else if(i%3==0 && i%5==0 && i%11==0){
+                System.out.println("CozaLoza ");
+            }else if(i%5==0 && i%7==0 && i%11==0){
+                System.out.println("LozaWoza ");
+            }else if(i%3==0 && i%7==0 && i%11==0){
+                System.out.println("CozaWoza ");
+            }else if(i%3==0 && i%11==0){
+                System.out.println("Coza ");
+            }else if(i%5==0 && i%11==0){
+                System.out.println("Loza ");
+            }else if(i%7==0 && i%11==0){
+                System.out.println("Woza ");
+            }else if(i%3==0 && i%5==0){
+                System.out.print("CozaLoza ");
+            }else if(i%5==0 && i%7==0){
+                System.out.print("LozaWoza ");
+            }else if(i%3==0 && i%7==0){
+                System.out.print("CozaWoza ");
+            }else if(i%3==0){
+                System.out.print("Coza ");
+            }else if(i%5==0){
+                System.out.print("Loza ");
+            }else if(i%7==0){
+                System.out.print("Woza ");
+            }else if(i%11==0){
+                System.out.println(" "+i+" ");
+            }else
+            System.out.print(" "+i+" ");
+        }
+    }
+}
